8c405ca158c3a1ca1bbe3b5975871639f97d3ee8
Camille C <camara.camille@gmail.com>
2022-07-31 11:30:20 +0200

    Project init

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="https://github.com/pulls">Pull Request</a>
+          </li>
+          <li>
+            <a href="https://github.com/issues">Pull Request</a>
+          </li>
+          <li>
+            <a href="https://github.com/marketplace">Pull Request</a>
+          </li>
+          <li>
+            <a href="https://github.com/explore">Pull Request</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
