5798682bec716fcded70b731d7fe74381fc86192
Camille C <camara.camille@gmail.com>
2022-08-02 12:45:49 +0200

    v2.01 console error fix

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -10,7 +10,7 @@ import { Dialog, Transition } from "@headlessui/react";
 import { RssIcon, XIcon } from "@heroicons/react/outline";
 import TriangleGrey from "./assets/icons/Octicons-triangle-down-grey.svg";
 
-function Dashboard() {
+const Dashboard = () => {
   const [isFollowing, setIsFollowing] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isMoreRepo, setIsMoreRepo] = useState(false);
@@ -37,7 +37,11 @@ function Dashboard() {
             <div className="pb-4 flex flex-col gap-2">
               {repositories.slice(0, isMoreRepo ? 20 : 8).map((repository) => {
                 return (
-                  <Repository name={repository.name} link={repository.link} />
+                  <Repository
+                    key={repository.name + repositories.indexOf(repository)}
+                    name={repository.name}
+                    link={repository.link}
+                  />
                 );
               })}
             </div>
@@ -81,7 +85,7 @@ function Dashboard() {
                   className="text-white"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     fill="white"
                     d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"
                   ></path>
@@ -111,7 +115,11 @@ function Dashboard() {
                 .slice(0, isMoreRepo ? repositories.length : 8)
                 .map((repository) => {
                   return (
-                    <Repository name={repository.name} link={repository.link} />
+                    <Repository
+                      key={repository.name + repositories.indexOf(repository)}
+                      name={repository.name}
+                      link={repository.link}
+                    />
                   );
                 })}
             </div>
@@ -247,7 +255,7 @@ function Dashboard() {
                                   class="octicon octicon-comment-discussion feed-filter-item-icon color-fg-muted mx-2"
                                 >
                                   <path
-                                    fill-rule="evenodd"
+                                    fillRule="evenodd"
                                     d="M1.5 2.75a.25.25 0 01.25-.25h8.5a.25.25 0 01.25.25v5.5a.25.25 0 01-.25.25h-3.5a.75.75 0 00-.53.22L3.5 11.44V9.25a.75.75 0 00-.75-.75h-1a.25.25 0 01-.25-.25v-5.5zM1.75 1A1.75 1.75 0 000 2.75v5.5C0 9.216.784 10 1.75 10H2v1.543a1.457 1.457 0 002.487 1.03L7.061 10h3.189A1.75 1.75 0 0012 8.25v-5.5A1.75 1.75 0 0010.25 1h-8.5zM14.5 4.75a.25.25 0 00-.25-.25h-.5a.75.75 0 110-1.5h.5c.966 0 1.75.784 1.75 1.75v5.5A1.75 1.75 0 0114.25 12H14v1.543a1.457 1.457 0 01-2.487 1.03L9.22 12.28a.75.75 0 111.06-1.06l2.22 2.22v-2.19a.75.75 0 01.75-.75h1a.25.25 0 00.25-.25v-5.5z"
                                   ></path>
                                 </svg>
@@ -272,7 +280,7 @@ function Dashboard() {
                                   class="octicon octicon-tag feed-filter-item-icon color-fg-muted mx-2"
                                 >
                                   <path
-                                    fill-rule="evenodd"
+                                    fillRule="evenodd"
                                     d="M2.5 7.775V2.75a.25.25 0 01.25-.25h5.025a.25.25 0 01.177.073l6.25 6.25a.25.25 0 010 .354l-5.025 5.025a.25.25 0 01-.354 0l-6.25-6.25a.25.25 0 01-.073-.177zm-1.5 0V2.75C1 1.784 1.784 1 2.75 1h5.025c.464 0 .91.184 1.238.513l6.25 6.25a1.75 1.75 0 010 2.474l-5.026 5.026a1.75 1.75 0 01-2.474 0l-6.25-6.25A1.75 1.75 0 011 7.775zM6 5a1 1 0 100 2 1 1 0 000-2z"
                                   ></path>
                                 </svg>
@@ -295,7 +303,7 @@ function Dashboard() {
                                   class="octicon octicon-heart feed-filter-item-icon color-fg-muted mx-2"
                                 >
                                   <path
-                                    fill-rule="evenodd"
+                                    fillRule="evenodd"
                                     d="M4.25 2.5c-1.336 0-2.75 1.164-2.75 3 0 2.15 1.58 4.144 3.365 5.682A20.565 20.565 0 008 13.393a20.561 20.561 0 003.135-2.211C12.92 9.644 14.5 7.65 14.5 5.5c0-1.836-1.414-3-2.75-3-1.373 0-2.609.986-3.029 2.456a.75.75 0 01-1.442 0C6.859 3.486 5.623 2.5 4.25 2.5zM8 14.25l-.345.666-.002-.001-.006-.003-.018-.01a7.643 7.643 0 01-.31-.17 22.075 22.075 0 01-3.434-2.414C2.045 10.731 0 8.35 0 5.5 0 2.836 2.086 1 4.25 1 5.797 1 7.153 1.802 8 3.02 8.847 1.802 10.203 1 11.75 1 13.914 1 16 2.836 16 5.5c0 2.85-2.045 5.231-3.885 6.818a22.08 22.08 0 01-3.744 2.584l-.018.01-.006.003h-.002L8 14.25zm0 0l.345.666a.752.752 0 01-.69 0L8 14.25z"
                                   ></path>
                                 </svg>
@@ -318,7 +326,7 @@ function Dashboard() {
                                   class="octicon octicon-star feed-filter-item-icon color-fg-muted mx-2"
                                 >
                                   <path
-                                    fill-rule="evenodd"
+                                    fillRule="evenodd"
                                     d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"
                                   ></path>
                                 </svg>
@@ -341,7 +349,7 @@ function Dashboard() {
                                   class="octicon octicon-repo feed-filter-item-icon color-fg-muted mx-2"
                                 >
                                   <path
-                                    fill-rule="evenodd"
+                                    fillRule="evenodd"
                                     d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"
                                   ></path>
                                 </svg>
@@ -367,7 +375,7 @@ function Dashboard() {
                                   class="octicon octicon-person-add feed-filter-item-icon color-fg-muted mx-2"
                                 >
                                   <path
-                                    fill-rule="evenodd"
+                                    fillRule="evenodd"
                                     d="M13.25 0a.75.75 0 01.75.75V2h1.25a.75.75 0 010 1.5H14v1.25a.75.75 0 01-1.5 0V3.5h-1.25a.75.75 0 010-1.5h1.25V.75a.75.75 0 01.75-.75zM5.5 4a2 2 0 100 4 2 2 0 000-4zm2.4 4.548a3.5 3.5 0 10-4.799 0 5.527 5.527 0 00-3.1 4.66.75.75 0 101.498.085A4.01 4.01 0 015.5 9.5a4.01 4.01 0 014.001 3.793.75.75 0 101.498-.086 5.527 5.527 0 00-3.1-4.659z"
                                   ></path>
                                 </svg>
@@ -479,7 +487,7 @@ function Dashboard() {
                       className="fill-textGrey"
                     >
                       <path
-                        fill-rule="evenodd"
+                        fillRule="evenodd"
                         d="M8 1.5c-2.363 0-4 1.69-4 3.75 0 .984.424 1.625.984 2.304l.214.253c.223.264.47.556.673.848.284.411.537.896.621 1.49a.75.75 0 01-1.484.211c-.04-.282-.163-.547-.37-.847a8.695 8.695 0 00-.542-.68c-.084-.1-.173-.205-.268-.32C3.201 7.75 2.5 6.766 2.5 5.25 2.5 2.31 4.863 0 8 0s5.5 2.31 5.5 5.25c0 1.516-.701 2.5-1.328 3.259-.095.115-.184.22-.268.319-.207.245-.383.453-.541.681-.208.3-.33.565-.37.847a.75.75 0 01-1.485-.212c.084-.593.337-1.078.621-1.489.203-.292.45-.584.673-.848.075-.088.147-.173.213-.253.561-.679.985-1.32.985-2.304 0-2.06-1.637-3.75-4-3.75zM6 15.25a.75.75 0 01.75-.75h2.5a.75.75 0 010 1.5h-2.5a.75.75 0 01-.75-.75zM5.75 12a.75.75 0 000 1.5h4.5a.75.75 0 000-1.5h-4.5z"
                       ></path>
                     </svg>
@@ -538,7 +546,7 @@ function Dashboard() {
                   className="absolute -left-2 -top-1"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     fill="#d0d7de"
                     d="M8 4a4 4 0 100 8 4 4 0 000-8z"
                   ></path>
@@ -562,7 +570,7 @@ function Dashboard() {
                   className="absolute -left-2 -top-1"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     fill="#d0d7de"
                     d="M8 4a4 4 0 100 8 4 4 0 000-8z"
                   ></path>
@@ -586,7 +594,7 @@ function Dashboard() {
                   className="absolute -left-2 -top-1"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     fill="#d0d7de"
                     d="M8 4a4 4 0 100 8 4 4 0 000-8z"
                   ></path>
@@ -611,7 +619,7 @@ function Dashboard() {
                   className="absolute -left-2 -top-1"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     fill="#d0d7de"
                     d="M8 4a4 4 0 100 8 4 4 0 000-8z"
                   ></path>
@@ -636,6 +644,6 @@ function Dashboard() {
       {/* MODAL */}
     </div>
   );
-}
+};
 
 export default Dashboard;
